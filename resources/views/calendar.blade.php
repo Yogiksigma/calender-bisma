9f8fe08826e918b3db22bf05c515e8ae704cb7ca
Yogiksigma <daniwarmawan@gmail.com>
2026-02-06 10:01:53 +0800

    second commit

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -10,6 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     
+    <!-- <link href="{{ asset('css/calendar.css') }}" rel="stylesheet"> ngelink css -->
     <style>
         body {
             padding: 20px;
